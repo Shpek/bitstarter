710d1dff4c511f5aeb58204fcbce1d483e70021e
Sergey Georgiev <sergei.georgiev@gmail.com>
2013-07-11 15:24:35 +0000

    Added grader.js

---
+++ grader.js
@@ -0,0 +1,103 @@
+#!/usr/bin/env node
+/*
+Automatically grade files for the presence of specified HTML tags/attributes.
+Uses commander.js and cheerio. Teaches command line application development
+and basic DOM parsing.
+
+References:
+
+ + cheerio
+   - https://github.com/MatthewMueller/cheerio
+   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
+   - http://maxogden.com/scraping-with-node.html
+
+ + commander.js
+   - https://github.com/visionmedia/commander.js
+   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
+
+ + JSON
+   - http://en.wikipedia.org/wiki/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+*/
+
+var fs = require('fs');
+var program = require('commander');
+var cheerio = require('cheerio');
+var rest = require('restler');
+var util = require('util');
+var HTMLFILE_DEFAULT = "index.html";
+var CHECKSFILE_DEFAULT = "checks.json";
+
+var assertFileExists = function(infile) {
+    var instr = infile.toString();
+    if(!fs.existsSync(instr)) {
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    return instr;
+};
+
+var cheerioHtmlFile = function(htmlfile) {
+    return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var loadChecks = function(checksfile) {
+    return JSON.parse(fs.readFileSync(checksfile));
+};
+
+var checkHtmlFile = function(htmlfile, checksfile) {
+    $ = cheerio.load(htmlfile);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
+var clone = function(fn) {
+    // Workaround for commander.js issue.
+    // http://stackoverflow.com/a/6772648
+    return fn.bind({});
+};
+
+var checkFile = function(fileName, checksFile) {
+    var contents = fs.readFileSync(fileName);
+    var res = checkHtmlFile(contents, checksFile);
+    dumpResult(res);
+}
+
+var checkUrl = function(url, checksFile) {
+    rest.get(url).on('complete', function(result, response) {
+	if (result instanceof Error) {
+	    console.error('Error: ' + util.format(result.code));
+	    process.exit(1);
+	} else {
+	    var res = checkHtmlFile(result, checksFile);
+	    dumpResult(res);
+	}
+    });
+}
+
+var dumpResult = function(json) {
+    var outJson = JSON.stringify(json, null, 4);
+    console.log(outJson);
+}
+
+if(require.main == module) {
+    program
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'URL to check', '')
+        .parse(process.argv);
+    if (program.url.length != 0) {
+	checkUrl(program.url, program.checks);
+    } else {
+	checkFile(program.file, program.checks);
+    }
+} else {
+    exports.checkUrl = checkUrl;
+    exports.checkFile = checkFile;
+}
